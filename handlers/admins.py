c1c6299253b1bb9b6ff9e950c4743b4678ef5f16
HeLLxGodLike <ysonu4951@gmail.com>
2021-04-06 19:43:55 +0530

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -38,7 +38,7 @@ async def resume(client: Client, message: Message):
 )
 @errors
 @admins_only
-async def stop(client: Client, message: Message):
+async def stopm(client: Client, message: Message):
     try:
         sira.clear(message.chat.id)
     except:
@@ -81,9 +81,9 @@ async def admincache(client, message: Message):
     await message.reply_text("❇️ Admin cache refreshed!")
 
 @Client.on_message(
-    filters.command("help")
+    filters.command("helpmusic")
     & filters.group
     & ~ filters.edited
 )
 async def helper(client , message:Message):
-     await message.reply_text("The commands and there use is explained here-: \n `/saavn` To search song on jio saavan and play the first result \n `/deezer` To search the song on deezer and get good quality stream \n `/ytt` To search the song on Youtube and play the first matching result \n '/play` Reply this in response to a link or any telegram audio file it will be played \n `/skip` to skip current song \n `/stop or /kill` to stop the streaming of song \n `/pause` to pause the stream \n `/resume` to resume the playback. \n Inline search is also supported.")
+     await message.reply_text("The commands and there use is explained here-: \n `/deezer` To search the song on deezer and get good quality stream \n `/ytt` To search the song on Youtube and play the first matching result \n '/play` Reply this in response to a link or any telegram audio file it will be played \n `/skip` to skip current song \n `/stop or /kill` to stop the streaming of song \n `/pause` to pause the stream \n `/resume` to resume the playback. \n Inline search is also supported.")
