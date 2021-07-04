70364bec50933fbe317cba8e0d1b79ce6775c8bf
HeLLxGodLike <ysonu4951@gmail.com>
2021-07-04 20:21:05 +0530

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -80,12 +80,6 @@ async def admincache(client, message: Message):
     set(message.chat.id, [member.user for member in await message.chat.get_members(filter="administrators")])
     await message.reply_text("❇️ Admin cache refreshed!")
 
-# @Client.on_message(filters.command("restartmusic") & filters.user(SUDO_USERS))
-# async def restart(c: Client, m: Message):
-#    await m.reply_text("Restarting...")
-#    args = [sys.executable, "main.py"]
-#    os.execl(sys.executable, *args)
-
 @Client.on_message(
     filters.command("helpmusic")
     & filters.group
