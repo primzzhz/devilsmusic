15a8eee15c0846faafaa780cb79b46c2737dc56f
HeLLxGodLike <ysonu4951@gmail.com>
2021-07-04 17:13:50 +0530

    Update __init__.py

--- services/queues/__init__.py
+++ services/queues/__init__.py
@@ -1,7 +1,7 @@
-from devilsmusic.services.queues.queues import clear 
-from devilsmusic.services.queues.queues import get
-from devilsmusic.services.queues.queues import is_empty
-from devilsmusic.services.queues.queues import put
-from devilsmusic.services.queues.queues import task_done
+from services.queues.queues import clear 
+from services.queues.queues import get
+from services.queues.queues import is_empty
+from services.queues.queues import put
+from services.queues.queues import task_done
 
 __all__ = ["clear", "get", "is_empty", "put", "task_done"]
