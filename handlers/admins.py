8129b9d20c4ebb71c4fb0b0ab4bdbe36ae6dcb71
HeLLxGodLike <ysonu4951@gmail.com>
2021-07-06 14:31:50 +0530

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -1,5 +1,3 @@
-import sys
-import os
 from pyrogram import Client, filters
 from pyrogram.types import Message
 import tgcalls
@@ -82,11 +80,11 @@ async def admincache(client, message: Message):
     set(message.chat.id, [member.user for member in await message.chat.get_members(filter="administrators")])
     await message.reply_text("❇️ Admin cache refreshed!")
 
-@Client.on_message(filters.command("restartmusic") & filters.user(SUDO_USERS))
-async def restart(c: Client, m: Message):
-   await m.reply_text("Restarting...")
-   args = [sys.executable, "main.py"]
-   os.execl(sys.executable, *args)
+# @Client.on_message(filters.command("restartmusic") & filters.user(SUDO_USERS))
+# async def restart(c: Client, m: Message):
+#   await m.reply_text("Restarting...")
+#   args = [sys.executable, "main.py"]
+#   os.execl(sys.executable, *args)
 
 @Client.on_message(
     filters.command("helpmusic")
