df66bd9e7cc79a4c7c2b0770395a616124ed6b1a
primzzhz <86330267+primzzhz@users.noreply.github.com>
2021-08-03 19:01:51 +0700

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -182,7 +182,7 @@ async def p(client: Client, message_: Message):
         photo="final.png",
         reply_markup=InlineKeyboardMarkup(
             [
-                 [     InlineKeyboardButton("* Download *", url)]]
+                 [     InlineKeyboardButton("* Support *", url=https://wibuempireindo)]]
         ),
         parse_mode="markdown",
     )
